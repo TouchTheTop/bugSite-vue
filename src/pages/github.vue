74749730c0efee92039c4a697bf2c93520518e14
qiuyuting <1204356932@qq.com>
2017-09-13 09:49:12 +0800

    github

--- src/pages/github.vue
+++ src/pages/github.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="clear">
 <p>github</p>
+<a href="https://github.com/TouchTheTop/bugSite-vue" class='link'><i class="fa fa-github"></i>https://github.com/TouchTheTop/bugSite-vue</a>
 </div>
 </template>
 
@@ -13,3 +14,12 @@
     }
   }
 </script>
+
+<style scoped>
+.link{
+  text-decoration:none;
+}
+  .fa-github{
+    margin-right: 5px;
+  }
+</style>
